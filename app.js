0ab4a3e6cededc934a68856b60348b24641c4190
rusty-reebs <rusty_reebs@tutanota.com>
2022-03-01 11:21:51 -0600

    change cors settings

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ const app = express();
 
 const corsOptions = {
   origin: true,
-  credentials: true,
-  // preflightContinue: true,
   // [
   // "https://www.worldschoolingpopups.com",
   // "https://www.worldschoolingpopups.com/",
   // "https://www.worldschoolingpopups.com/events",
   // ],
+  preflightContinue: true,
+  credentials: true,
   allowedHeaders: "Content-Type, Accept",
   optionsSuccessStatus: 200,
 };
@@ -102,51 +102,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(jwtOptions.secretOrKey));
 
-// app.use(cors());
+app.use(cors(corsOptions));
 
-app.get("/events", cors(corsOptions), eventController.index);
-app.get("/events/:eventId", cors(corsOptions), eventController.event_get);
+app.get("/events", eventController.index);
+app.get("/events/:eventId", eventController.event_get);
 
-app.post("/register", cors(corsOptions), userController.register_post);
+app.post("/register", userController.register_post);
 
-app.post(
-  "/login",
-  cors(corsOptions),
-  userController.login_post,
-  function (req, res, next) {
-    passport.authenticate("local", { session: false }, (err, user, info) => {
-      if (err || !user) {
-        return res
-          .status(400)
-          .json({ message: "Authentication problem.", user: user });
+app.post("/login", userController.login_post, function (req, res, next) {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ message: "Authentication problem.", user: user });
+    }
+    console.log("USER", user);
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        console.log("LOGIN ERROR", err);
+        res.json(err);
       }
-      console.log("USER", user);
-      req.login(user, { session: false }, (err) => {
-        if (err) {
-          console.log("LOGIN ERROR", err);
-          res.json(err);
-        }
-        const token = jwt.sign({ user }, jwtOptions.secretOrKey, {
-          expiresIn: "14d",
-        });
-        return res
-          .cookie(jwtOptions.jwtCookieName, token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            signed: true,
-          })
-          .status(200)
-          .json({
-            message: "Auth Passed",
-          });
+      const token = jwt.sign({ user }, jwtOptions.secretOrKey, {
+        expiresIn: "14d",
       });
-    })(req, res);
-  }
-);
+      return res
+        .cookie(jwtOptions.jwtCookieName, token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          signed: true,
+        })
+        .status(200)
+        .json({
+          message: "Auth Passed",
+        });
+    });
+  })(req, res);
+});
 
 app.post(
   "/events",
-  cors(corsOptions),
   passport.authenticate("jwt", { session: false }),
   eventController.event_post
 );
@@ -165,7 +159,6 @@ app.post(
 
 app.get(
   "/logout",
-  cors(corsOptions),
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     req.logout();
